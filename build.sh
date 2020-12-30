5f7a01cf3d7eb06f26f47c85b5e7f4d595d9ac77
Harsha Raghu <funnyboy313@gmail.com>
2020-12-30 19:33:26 +0530

    Update build.sh
    Add config.h into packages

--- build.sh
+++ build.sh
@@ -68,6 +68,9 @@ BUILD_NAME="ffmpeg-$(./ffbuild/ffmpeg/ffbuild/version.sh ffbuild/ffmpeg)-${TARGE
 mkdir -p "ffbuild/pkgroot/$BUILD_NAME"
 package_variant ffbuild/prefix "ffbuild/pkgroot/$BUILD_NAME"
 
+cp -f "ffbuild/prefix/config.h" "ffbuild/pkgroot/$BUILD_NAME/include/config.h"
+
+
 cd ffbuild/pkgroot
 zip -9 -r "${ARTIFACTS_PATH}/${BUILD_NAME}.zip" "$BUILD_NAME"
 cd -
